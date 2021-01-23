3343da1a6f6745b286dba8ad5c078da599c1ba7a
Immijimmi <immijimmi1@gmail.com>
2021-01-23 03:39:19 +0000

    Provided more in-depth explanation of __hash__ disabling

--- managedstate/propertyName.py
+++ managedstate/propertyName.py
@@ -1,5 +1,9 @@
 class PropertyName:
-    __hash__ = None  # Hash for this class will need custom implementation, not currently implemented
+    __hash__ = None
+    """
+    As this class is used indirectly to determine the method of access for the state object,
+    it will never be used as a direct index. Thus, it should never be set as a key.
+    """
 
     def __init__(self, property_name: str):
         self.__name = property_name
