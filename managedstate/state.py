88ce37994c94526176b6893af2f0d8e1a6718c7e
Immijimmi <immijimmi1@gmail.com>
2021-01-23 08:42:08 +0000

    Added docstrings

--- managedstate/state.py
+++ managedstate/state.py
@@ -10,9 +10,23 @@ class State(Extendable):
         self.__state = initial_state or {}
 
     def get(self, path_keys: Sequence[Any] = (), defaults: Sequence[Any] = ()) -> Any:
+        """
+        Drills into the state object using the provided path keys in sequence.
+        Any time progressing further into the state object fails, the default value at the relevant index of defaults
+        is substituted in.
+        Once all path keys have been applied, the drilled-down state object is returned
+        """
+
         return RecursiveNode(self.__state, path_keys, defaults).get()
 
     def set(self, value: Any, path_keys: Sequence[Any] = (), defaults: Sequence[Any] = ()) -> None:
+        """
+        Drills into the state object using the provided path keys in sequence.
+        Any time progressing further into the state object fails, the default value at the relevant index of defaults
+        is substituted in.
+        Once all path keys have been applied, the current subset of the state object is set equal to the provided value
+        """
+
         working_state = RecursiveNode(self.__state, path_keys, defaults)
         working_state.set(value)
         self.__state = working_state.value
