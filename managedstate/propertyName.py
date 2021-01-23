3cb1c5dbe83f0a48018818909876fe87c4d0c0ee
Immijimmi <immijimmi1@gmail.com>
2021-01-23 03:52:24 +0000

    Added class docstring

--- managedstate/propertyName.py
+++ managedstate/propertyName.py
@@ -1,4 +1,9 @@
 class PropertyName:
+    """
+    An instance of this class should be provided as a path key when getting or setting the state,
+    to indicate that the next level of the state should be accessed via an object attribute
+    """
+
     __hash__ = None
     """
     As this class is used indirectly to determine the method of access for the state object,
