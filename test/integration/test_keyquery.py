9555ad462d8575ea247c4c69ebd3a9aab74eab9b
Immijimmi <immijimmi1@gmail.com>
2021-01-26 09:53:55 +0000

    Added KeyQuery tests

---
+++ test/integration/test_keyquery.py
@@ -0,0 +1,19 @@
+from managedstate import State, KeyQuery
+
+
+class TestKeyQuery:
+    def test_get_uses_keyquery_correctly(self, res):
+        state = State(res.value_2)
+
+        query = KeyQuery(lambda substate: substate.index({"key_2": 5}))
+
+        assert state.get(["key_1", query, "key_2"]) == 5
+
+    def test_set_uses_keyquery_correctly(self, res):
+        state = State(res.value_2)
+
+        query = KeyQuery(lambda substate: substate.index({"key_2": 5}))
+
+        state.set("value", ["key_1", query, "key_2"])
+
+        assert state.get(["key_1", 1, "key_2"]) == "value"
