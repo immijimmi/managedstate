939a39d7476ee9ebd1151e078298dfffe8434d17
Immijimmi <immijimmi1@gmail.com>
2021-01-26 10:42:56 +0000

    Added PartialQuery tests

---
+++ test/integration/test_partialquery.py
@@ -0,0 +1,35 @@
+import pytest
+
+from managedstate import State, KeyQuery
+from managedstate.extensions import Registrar, PartialQuery
+
+
+class TestPartialQuery:
+    def test_partialquery_without_args_raises_error(self, res):
+        state = State.with_extensions(Registrar)(res.value_2)
+
+        query = PartialQuery(lambda args: 0)
+
+        state.register("label", ["key_1", query])
+
+        assert pytest.raises(IndexError, state.registered_get, "label")
+        assert pytest.raises(IndexError, state.registered_set, "value", "label")
+
+    def test_registered_get_with_partialquery(self, res):
+        state = State.with_extensions(Registrar)(res.value_2)
+
+        query = PartialQuery(lambda args: KeyQuery(lambda substate: args))
+
+        state.register("label", ["key_1", query, "key_2"])
+
+        assert state.registered_get("label", (1,)) == 5
+
+    def test_registered_set_last_key_partialquery(self, res):
+        state = State.with_extensions(Registrar)(res.value_2)
+
+        query = PartialQuery(lambda args: KeyQuery(lambda substate: "key"+args))
+
+        state.register("label", ["key_1", 0, query])
+        state.registered_set("value", "label", ("_2",))
+
+        assert state.get(["key_1", 0, "key_2"]) == "value"
